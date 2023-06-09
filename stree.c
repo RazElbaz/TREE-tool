aa8e37be85501d7499037146f49aef079f10989b
Raz Elbaz <93310416+RazElbaz@users.noreply.github.com>
2023-06-09 12:55:05 +0300

    Update stree.c

--- stree.c
+++ stree.c
@@ -1,62 +1,28 @@
-/* nftw_dir_tree.c
-   Demonstrate the use of nftw(3). Walk though the directory tree specified
-   on the command line (or the current working directory if no directory
-   is specified on the command line), displaying an indented hierarchy
-   of files in the tree. For each file, display:
-      * a letter indicating the file type (using the same letters as "ls -l")
-      * a string indicating the file type, as supplied by nftw()
-      * the file's i-node number.
-*/
 #define _XOPEN_SOURCE 600 /* Get nftw() */
 #include <ftw.h>
-#include <sys/types.h>    /* Type definitions used by many programs */
-#include <stdio.h>        /* Standard I/O functions */
-#include <stdlib.h>       /* Prototypes of commonly used library functions,
-                             plus EXIT_SUCCESS and EXIT_FAILURE constants */
-#include <unistd.h>       /* Prototypes for many system calls */
-#include <errno.h>        /* Declares errno and defines error constants */
-#include <string.h>       /* Commonly used string-handling functions */
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <errno.h>
+#include <string.h>
+#include <dirent.h>
+#include <sys/stat.h>
+#include <sys/types.h>
+#include <pwd.h>
+#include <grp.h>
 
-static int              /* Callback function called by ftw() */
-dirTree(const char *pathname, const struct stat *sbuf, int type, struct FTW *ftwb)
-{
-    if (type == FTW_NS) {                  /* Could not stat() file */
-        printf("?");
-    } else {
-        switch (sbuf->st_mode & S_IFMT) {  /* Print file type */
-        case S_IFREG:  printf("-"); break;
-        case S_IFDIR:  printf("d"); break;
-        case S_IFCHR:  printf("c"); break;
-        case S_IFBLK:  printf("b"); break;
-        case S_IFLNK:  printf("l"); break;
-        case S_IFIFO:  printf("p"); break;
-        case S_IFSOCK: printf("s"); break;
-        default:       printf("?"); break; /* Should never happen (on Linux) */
-        }
-    }
+typedef struct counter {
+    size_t dirs;
+    size_t files;
+} counter_t;
 
-    if (type != FTW_NS)
-        printf("%7ld ", (long) sbuf->st_ino);
-    else
-        printf("        ");
-	
-    printf(" %*s", 4 * ftwb->level, " ");         /* Indent suitably */
-    printf("%s\n",  &pathname[ftwb->base]);     /* Print basename */
-    return 0;                                   /* Tell nftw() to continue */
-}
+typedef struct file_info {
+    char *name;
+    int is_dir;
+    struct file_info *next;
+} file_info_t;
 
-int
-main(int argc, char *argv[])
-{
-    int flags = 0;
-    if (argc != 2) {
-        fprintf(stderr, "Usage: %s directory-path\n", argv[0]);
-        exit(EXIT_FAILURE);
-    }
-
-    if (nftw(argv[1], dirTree, 10, flags) == -1) {
-        perror("nftw");
-        exit(EXIT_FAILURE);
-    }
-    exit(EXIT_SUCCESS);
-}
+void print_indentation(int depth);
+int is_jpg_png_gif(const char* filename);
+int is_tar_gz_or_zip(const char* filename);
+void walk(const char* directory, const char* prefix, counter_t *counter, int depth);
