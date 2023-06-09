afd4bf1685a5c600aae141b31c1840e848ba374c
Raz Elbaz <93310416+RazElbaz@users.noreply.github.com>
2023-06-09 13:25:38 +0300

    Update stree.c

--- stree.c
+++ stree.c
@@ -12,139 +12,197 @@
 #include <grp.h>
 
 typedef struct counter {
-    size_t dirs;
-    size_t files;
+  size_t dirs;
+  size_t files;
 } counter_t;
 
 typedef struct file_info {
-    char *name;
-    int is_dir;
-    struct file_info *next;
+  char *name;
+  int is_dir;
+  struct file_info *next;
 } file_info_t;
 
 void print_indentation(int depth) {
-    for (int i = 0; i < depth; i++) {
-        if (i == depth - 1) {
-            printf(" ");
-        } else {
-            printf("│   ");
-        }
+  for (int i = 0; i < depth; i++) {
+    if (i == depth - 1) {
+      printf(" ");
+    } else {
+      printf("   ");
     }
+  }
 }
-
 int is_jpg_png_gif(const char* filename) {
-    const char* extension = strrchr(filename, '.');
-    if (extension != NULL) {
-        if (strcmp(extension, ".jpg") == 0 ||
-            strcmp(extension, ".png") == 0 ||
-            strcmp(extension, ".gif") == 0) {
-            return 1;
-        }
+  const char* extension = strrchr(filename, '.');
+  if (extension != NULL) {
+    if (strcmp(extension, ".jpg") == 0 ||
+        strcmp(extension, ".png") == 0 ||
+        strcmp(extension, ".gif") == 0) {
+      return 1;
     }
-    return 0;
+  }
+  return 0;
 }
-
 int is_tar_gz_or_zip(const char* filename) {
-    const char* extension = strrchr(filename, '.');
-    if (extension != NULL) {
-        if (strcmp(extension, ".gz") == 0 ||
-            strcmp(extension, ".zip") == 0) {
-            return 1;
-        }
+  const char* extension = strrchr(filename, '.');
+  if (extension != NULL) {
+    if (strcmp(extension, ".gz") == 0 ||
+        strcmp(extension, ".zip") == 0) {
+      return 1;
     }
-    return 0;
+  }
+  return 0;
 }
-
 void walk(const char* directory, const char* prefix, counter_t *counter, int depth) {
-    file_info_t *head = NULL, *current, *iter;
-    size_t size = 0, index;
+  file_info_t *head = NULL, *current, *iter;
+  size_t size = 0, index;
 
-    struct dirent *file_dirent;
-    DIR *dir_handle;
+  struct dirent *file_dirent;
+  DIR *dir_handle;
 
-    char *full_path, *segment, *pointer, *next_prefix;
+  char *full_path, *segment, *pointer, *next_prefix;
 
-    dir_handle = opendir(directory);
-    if (!dir_handle) {
-        fprintf(stderr, "Cannot open directory \"%s\"\n", directory);
-        return;
+  dir_handle = opendir(directory);
+  if (!dir_handle) {
+    fprintf(stderr, "Cannot open directory \"%s\"\n", directory);
+    return;
+  }
+
+  counter->dirs++;
+
+  while ((file_dirent = readdir(dir_handle)) != NULL) {
+    if (file_dirent->d_name[0] == '.') {
+      continue;
     }
 
-    counter->dirs++;
+    current = malloc(sizeof(file_info_t));
+    current->name = strcpy(malloc(strlen(file_dirent->d_name) + 1), file_dirent->d_name);
 
-    while ((file_dirent = readdir(dir_handle)) != NULL) {
-        if (file_dirent->d_name[0] == '.') {
-            continue;
-        }
+    struct stat file_stat;
+    full_path = malloc(strlen(directory) + strlen(current->name) + 2);
+    sprintf(full_path, "%s/%s", directory, current->name);
 
-        current = malloc(sizeof(file_info_t));
-        current->name = strcpy(malloc(strlen(file_dirent->d_name) + 1), file_dirent->d_name);
-
-        struct stat file_stat;
-        full_path = malloc(strlen(directory) + strlen(current->name) + 2);
-        sprintf(full_path, "%s/%s", directory, current->name);
-
-        if (stat(full_path, &file_stat) == -1) {
-            fprintf(stderr, "Failed to get stat for file \"%s\": %s\n", full_path, strerror(errno));
-            free(full_path);
-            free(current->name);
-            free(current);
-            continue;
-        }
-
-        if (S_ISDIR(file_stat.st_mode)) {
-            current->is_dir = 1;
-            counter->dirs++;
-        } else {
-            current->is_dir = 0;
-            counter->files++;
-        }
-
-        size += file_stat.st_blocks;
-
-        if (head == NULL || strcmp(head->name, current->name) > 0) {
-            current->next = head;
-            head = current;
-        } else {
-            iter = head;
-            while (iter->next != NULL && strcmp(iter->next->name, current->name) < 0) {
-                iter = iter->next;
-            }
-            current->next = iter->next;
-            iter->next = current;
-        }
-
-        free(full_path);
+    if (stat(full_path, &file_stat) == -1) {
+      fprintf(stderr, "Cannot get file information for \"%s\"\n", full_path);
+      free(full_path);
+      free(current->name);
+      free(current);
+      continue;
     }
 
-    closedir(dir_handle);
+    current->is_dir = S_ISDIR(file_stat.st_mode);
+    current->next = NULL;
 
-    print_indentation(depth);
-    printf("%s\n", prefix);
+    if (head == NULL) {
+      head = current;
+    } else if (strcmp(current->name, head->name) < 0) {
+      current->next = head;
+      head = current;
+    } else {
+      for (iter = head; iter->next && strcmp(current->name, iter->next->name) > 0; iter = iter->next);
 
-    for (iter = head; iter != NULL; iter = iter->next) {
-        print_indentation(depth);
-        printf("├── %s", iter->name);
-        if (iter->is_dir) {
-            printf("/");
-        }
-        printf("\n");
-
-        if (iter->is_dir) {
-            next_prefix = malloc(strlen(prefix) + strlen(iter->name) + 2);
-            sprintf(next_prefix, "%s%s/", prefix, iter->name);
-            walk(full_path, next_prefix, counter, depth + 1);
-            free(next_prefix);
-        }
+      current->next = iter->next;
+      iter->next = current;
     }
 
-    while (head != NULL) {
-        current = head;
-        head = head->next;
-        free(current->name);
-        free(current);
+    size++;
+  }
+
+  closedir(dir_handle);
+  if (!head) {
+    return;
+  }
+
+  for (index = 0; index < size; index++) {
+    if (index == size - 1) {
+      pointer = "└── ";
+      segment = "    ";
+    } else {
+      pointer = "├── ";
+      segment = "│  ";
     }
+
+    struct stat file_stat;
+    full_path = malloc(strlen(directory) + strlen(head->name) + 2);
+    sprintf(full_path, "%s/%s", directory, head->name);
+
+    if (stat(full_path, &file_stat) == -1) {
+      fprintf(stderr, "Cannot get file information for \"%s\"\n", full_path);
+      free(full_path);
+      continue;
+    }
+
+    int is_dir = head->is_dir;
+    char *name = head->name;
+
+    struct passwd *owner = getpwuid(file_stat.st_uid);
+    char *owner_name = owner ? owner->pw_name : "";
+    struct group *group = getgrgid(file_stat.st_gid);
+    char *group_name = group ? group->gr_name : "";
+
+    char permissions[11];
+    sprintf(permissions, "%s%s%s%s%s%s%s%s%s%s",
+            (S_ISDIR(file_stat.st_mode)) ? "d" : "-",
+            (file_stat.st_mode & S_IRUSR) ? "r" : "-",
+            (file_stat.st_mode & S_IWUSR) ? "w" : "-",
+            (file_stat.st_mode & S_IXUSR) ? "x" : "-",
+            (file_stat.st_mode & S_IRGRP) ? "r" : "-",
+            (file_stat.st_mode & S_IWGRP) ? "w" : "-",
+            (file_stat.st_mode & S_IXGRP) ? "x" : "-",
+            (file_stat.st_mode & S_IROTH) ? "r" : "-",
+            (file_stat.st_mode & S_IWOTH) ? "w" : "-",
+            (file_stat.st_mode & S_IXOTH) ? "x" : "-");
+
+printf("%s", prefix);
+print_indentation(depth);
+printf("%s[%s %-8s %-8s %10ld]  ", pointer, permissions, owner_name, group_name, file_stat.st_size);
+if (is_dir) {
+  printf("\033[1;34m%s\033[0m\n", name);  // Blue color for directories
+} else {
+  const char* extension = strrchr(name, '.');  // Declare the 'extension' variable here
+  if (S_ISSOCK(file_stat.st_mode)) {
+    printf("\033[1;93m%s\033[0m\n", name);  // Bright yellow color for socket files
+  } else if (S_ISFIFO(file_stat.st_mode)) {
+    printf("\033[0;33m%s\033[0m\n", name);  // Brown color for pipes
+  } else if (file_stat.st_mode & S_IXUSR) {
+    printf("\033[1;32m%s\033[0m\n", name);  // Green color for executable files
+  } else if (S_ISBLK(file_stat.st_mode)) {
+    printf("\033[1;33m%s\033[0m\n", name);  // Dark yellow color for block device files
+  } else if (S_ISCHR(file_stat.st_mode)) {
+    printf("\033[1;33m%s\033[0m\n", name);  // Dark yellow color for character device files
+  } else if (is_jpg_png_gif(name)) {
+    printf("\033[1;35m%s\033[0m\n", name);  // Purple color for jpg/png/gif files
+  } else if (S_ISREG(file_stat.st_mode) && access(full_path, F_OK) == -1) {
+    printf("\033[1;31m%s\033[0m\n", name);  // Red color for orphaned symbolic links
+  } else if (is_tar_gz_or_zip(name)) {
+    printf("\033[1;31m%s\033[0m\n", name);  // Red color for .tar.gz and .zip files
+  } else {
+    printf("%s\n", name);
+  }
+  
+  counter->files++;
 }
+
+
+
+
+    if (is_dir) {
+      next_prefix = malloc(strlen(prefix) + strlen(segment) + 1);
+      sprintf(next_prefix, "%s%s", prefix, segment);
+
+      walk(full_path, next_prefix, counter, depth + 1);
+      free(next_prefix);
+    }
+
+    free(full_path);
+
+    current = head;
+    head = head->next;
+
+    free(current->name);
+    free(current);
+  }
+}
+
 int main(int argc, char *argv[]) {
   if (argc != 2) {
     fprintf(stderr, "Usage: %s directory-path\n", argv[0]);
@@ -178,3 +236,4 @@ int main(int argc, char *argv[]) {
 
   return 0;
 }
+
